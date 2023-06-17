8ecc042564eaa5ee2a34ba915e6b3ae4c1da66da
Ahmed1983Continue <97775646+AhmedWithJSDevelopment@users.noreply.github.com>
2023-06-17 18:13:33 +0300

    ALMOOSHRQ

---
+++ review.jsx
@@ -0,0 +1,18 @@
+  export const reviews = [{
+                id: 1,
+                name: "Paul Carse",
+                job: " CTO and Co-founder of Life Moments ",
+                // img: "pers.png",
+                iframe: "https://www.youtube.com/embed/V0jW2k2840Q",
+                text: "«We now have a core team of engineers at Relevant who work for us full-time and are supplemented by 4 or 5 engineers with different skillsets when and if required.»",
+            }, {
+                id: 2,
+                name: "Cassian Harrison",
+                job: " Co-founder of Splink Industries and My Theory Test App by James May ",
+                // img: "https://cdn-cfdja.nitrocdn.com/uOxxxDWjcukgNxoRlWCAPnOZsfFUWQlA/assets/static/optimized/rev-6c1cefb/wp-content/uploads/1612272389607.jpeg",
+                iframe: "https://www.youtube.com/embed/tUU7OMbWGp4",
+
+                text: "“Working with you guys has been fantastic. The level of expertise of your development team is as good as the people that we get in the UK. You’ve got a fantastic talent base of programmers. It’s more challenging to find people of that quality in the UK.”",
+            },
+
+        ];
